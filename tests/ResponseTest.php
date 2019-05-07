26bcc1c497923699ecc09780f2a1fc077827dcb3
andyexeter <palmer.andy@gmail.com>
2019-05-07 13:06:38 +0100

    Add phpunit and tests

---
+++ tests/ResponseTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Palmtree\Curl\Tests;
+
+use Palmtree\Curl\Response;
+use PHPUnit\Framework\TestCase;
+
+class ResponseTest extends TestCase
+{
+    public function testParse()
+    {
+        $headers = [
+            'Date'          => 'Tue, 07 May 2019 11:30:00 GMT',
+            'Expires'       => '-1',
+            'Cache-Control' => 'private, max-age=0',
+            'Content-Type'  => 'text/html; charset=utf-8',
+            'Vary'          => 'Accept-Encoding',
+        ];
+
+        $rawResponse = 'HTTP/1.1 200 OK' . "\r\n";
+
+        foreach ($headers as $key => $value) {
+            $rawResponse .= "$key: $value\r\n";
+        }
+
+        $rawResponse .= "\r\nHello, World!";
+
+        $response = new Response($rawResponse);
+
+        foreach ($headers as $key => $value) {
+            $this->assertSame($value, $response->getHeader($key));
+        }
+
+        $this->assertSame('Hello, World!', $response->getBody());
+    }
+
+    public function testParseHttpContinue()
+    {
+        $headers = [
+            'Date'          => 'Tue, 07 May 2019 11:30:00 GMT',
+            'Expires'       => '-1',
+            'Cache-Control' => 'private, max-age=0',
+            'Content-Type'  => 'text/html; charset=utf-8',
+            'Vary'          => 'Accept-Encoding',
+        ];
+
+        $rawResponse = "HTTP/1.1 100 Continue\r\n\r\n";
+
+        $rawResponse .= "HTTP/1.1 200 OK\r\n";
+
+        foreach ($headers as $key => $value) {
+            $rawResponse .= "$key: $value\r\n";
+        }
+
+        $rawResponse .= "\r\nHello, World!";
+
+        $response = new Response($rawResponse);
+
+        foreach ($headers as $key => $value) {
+            $this->assertSame($value, $response->getHeader($key));
+        }
+
+        $this->assertSame('Hello, World!', $response->getBody());
+    }
+}
